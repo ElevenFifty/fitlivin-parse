e08f3a1aa8021ff6b1cf441ea19acc8d42f5da5c
Terrence Kunstek <terrence@kunstek.com>
2017-01-13 14:38:37 -0500

    Fix for email

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ var api = new ParseServer({
       // Your API key from mailgun.com
       apiKey: process.env.MG_API_KEY || '',
     },
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
