b948d6f6d1e1ddd7611061aebd5261cb1d9017b8
Terrence Kunstek <terrence@kunstek.com>
2017-01-30 14:24:49 -0500

    Adding cloud code to send push

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,31 @@
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+// targetUser = "hEwk3OS9Pp", alert="Hello World"
+Parse.Cloud.define('sendPush', function(request, response) {
+
+  var recipientUser = new Parse.User();
+  recipientUser.id = request.params.targetUser;
+
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("user", recipientUser);
+
+  var data = {
+    aps: {
+      alert: request.params.title,
+      badge:1
+    }
+  };
+
+  Parse.Push.send(
+      { where: pushQuery, data: data },
+      { useMasterKey: true,
+        success: function() {
+          // console.log("Successfully sent the push");
+      }, error: function(err) {
+          console.log(err);
+      }
+  }).then(function() {
+      response.success("Push Sent!");
+    }, function(error) {
+      response.error("Error while trying to send push " + error.message);
+    });
 });
