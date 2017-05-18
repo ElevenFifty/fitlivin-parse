8b4652b4388898c5e48bc1d1356438a6b533027f
Terrence Kunstek <terrence@kunstek.com>
2017-05-18 15:01:33 -0400

    Adding mark all read function

--- cloud/main.js
+++ cloud/main.js
@@ -33,3 +33,35 @@ Parse.Cloud.define('sendPush', function(request, response) {
       response.error("Error while trying to send push " + error.message);
     });
 });
+
+Parse.Cloud.define('markAllRead', function(request,response) {
+  var theUser = request.user;
+  var batchUpdate = [];
+
+  var noteQuery = new Parse.Query("Notification");
+  query.equalTo("user", theUser);
+  query.notEqualTo("read", true);
+  query.limit(1000);
+
+  query.find().then( function(result) {
+    result.forEach(function(note) {
+      note.set("read", true);
+      batchUpdate.push(note);
+    },
+    function(error) {});
+  }).then(function(foo) {
+    if(batchUpdate.length > 0) {
+      Parse.Object.saveAll(batchUpdate).then(
+        function(list) {
+			      // All the objects were saved.
+						console.log("all notes marked read");
+			    },
+			    function(error) {
+			      // An error occurred while saving one of the objects.
+						console.log(error);
+			    }
+				);
+    }
+  });
+
+});
